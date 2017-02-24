d645c5704bda32ccd5475d64e49165f922786274
Takushi HASHIDA <hashida.takushi@jp.fujitsu.com>
2017-02-24 16:28:26 +0900

    world map :+1:

--- scripts/worldmap.js
+++ scripts/worldmap.js
@@ -1,40 +1,75 @@
 var width = 1000;
 var height = 1000;
 
-// add SVG
-var svg = d3.select("body")
-    .append("svg")
+var div = d3.select("body")
+    .append("div")
     .attr("class", "world")
     .attr("width", width)
     .attr("height", height);
 
-// mercator projection
-var mercator = d3.geo.mercator()
-    .center([0,0])
-    .translate([width/2, height/2])
-    .scale(100);
+// leaflet
+var map = new L.Map(d3.select('div').node())
+    .setView([0, 0], 1);
 
-// convert getjson path object to svg path
-var geopath = d3.geo.path()
-    .projection(mercator);
+var tile = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+var svgLayer =  d3.select(map.getPanes().overlayPane).append('svg').attr('class', 'leaflet-zoom-hide');
 
 // color mapper
 var color = d3.scale.category20();
 
+var style = function (feature) {
+    return {
+        fillColor: color(feature.properties.pop_est),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.5
+    };
+};
+
+var geojson;
+
+function highlightFeature(e) {
+    var layer =  e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '3',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature,  layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
 // "https://d3js.org/world-110m.v1.json"
 // "http://luke2.zetta.flab.fujitsu.co.jp:3000/assets/world-50m.json"
-d3.json("http://luke2.zetta.flab.fujitsu.co.jp:3000/assets/world-50m.json", function(error, world) {
-    console.log(world);
-    var land = topojson.feature(world, world.objects.countries);
-    console.log(land);
-
-    svg.selectAll("path")
-	.data(land.features)
-	.enter().append("path")
-	.attr("class",  function(d) { return d.id; })
-	.attr("d", geopath)
-	.attr("fill", function(d) {
-	    return color(d.geometry.coordinates);
-	});
+d3.json("http://luke2.zetta.flab.fujitsu.co.jp:3000/assets/world-50m_geo.json", function(error, world) {
+    geojson = L.geoJson(world, {
+	style: style,
+	onEachFeature: onEachFeature
+    }).addTo(map);
 });
 
