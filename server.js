5df8ab3ec478da3f12eea51bd586f5fe89869296
Takushi HASHIDA <hashida.takushi@jp.fujitsu.com>
2017-03-05 17:08:36 +0000

    added tooltip

--- server.js
+++ server.js
@@ -126,6 +126,73 @@ server.route({
 
 server.route({
     method: 'GET',
+    path: '/api/q9',
+    handler: function (request, reply) {
+
+/////////////////////
+	var  q9_tmp = `
+	explain (analyze, format json, costs true, buffers true, verbose true )
+select
+	nation,
+	o_year,
+	sum(amount) as sum_profit
+from
+	(
+		select
+			n_name as nation,
+			extract(year from o_orderdate) as o_year,
+			l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount
+		from
+			part,
+			supplier,
+			lineitem,
+			partsupp,
+			orders,
+			nation
+		where
+			s_suppkey = l_suppkey
+			and ps_suppkey = l_suppkey
+			and ps_partkey = l_partkey
+			and p_partkey = l_partkey
+			and o_orderkey = l_orderkey
+			and s_nationkey = n_nationkey
+			and p_name like '%green%'
+	) as profit
+group by
+	nation,
+	o_year
+order by
+	nation,
+	o_year desc
+LIMIT 1;
+`;
+/////////////////////
+
+	// connect to our database
+	var client = new Pg.Client(config);
+	client.connect(function (err) {
+	    if (err) throw err;
+
+	    // execute a query on our database
+	    client.query(q9_tmp, function (err, result) {
+		if (err) throw err;
+
+		// just print the result to the console
+		// console.log(result.rows);
+		reply(result.rows);
+
+		// disconnect the client
+		client.end(function (err) {
+		    if (err) throw err;
+		});
+	    });
+	});
+    }
+});
+
+
+server.route({
+    method: 'GET',
     path: '/api/parm_test',
     handler: function (request, reply){
 	reply({greeting : 'hello ' + request.query.name });
